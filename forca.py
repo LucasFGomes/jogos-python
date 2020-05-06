ac97db79d15a6110c122d2443f8ec9c9ce13112f
Lucas Ferreira <lucasgomm33@gmail.com>
2020-05-06 01:03:15 -0300

    start implements hangman game

--- forca.py
+++ forca.py
@@ -3,6 +3,21 @@ def jogar():
     print("** Bem vindo ao jogo da Forca! **")
     print("*********************************")
 
+    palavra_escreta = 'lucas'
+    enforcou = False
+    acertou  = False
+
+    while(not enforcou and not acertou):
+
+        chute = input('Digite uma letra: ')
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_escreta:
+            if (chute.upper() == letra.upper()):
+                print("A letra {} está na {} posição".format(letra, index))
+            index += 1
+
     print("Fim de jogo")
 
 if __name__ == "__main__":
