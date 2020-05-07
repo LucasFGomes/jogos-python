a36c8b0f98a8a67493989570362375fe355b109a
Lucas Ferreira <lucasgomm33@gmail.com>
2020-05-07 01:48:15 -0300

    increasing the hangman's game

--- forca.py
+++ forca.py
@@ -3,22 +3,36 @@ def jogar():
     print("** Bem vindo ao jogo da Forca! **")
     print("*********************************")
 
-    palavra_escreta = 'lucas'
+    palavra_secreta = 'banana'
+    lista_letras_acertadas = []
     enforcou = False
     acertou  = False
+    tamanho_palavra_secreta = len(palavra_secreta)
+
+    print(f'A palavra secreta tem {tamanho_palavra_secreta} letras, Boa Sorte!')
+    for letra in range(0, tamanho_palavra_secreta):
+        lista_letras_acertadas.append("_")
+
+    print(formatar_lista(lista_letras_acertadas))
 
     while(not enforcou and not acertou):
-
-        chute = input('Digite uma letra: ')
+        chute = input('\nDigite uma letra: ')
         chute = chute.strip()
 
         index = 0
-        for letra in palavra_escreta:
+        for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
-                print("A letra {} está na {} posição".format(letra, index))
+                lista_acertadas[index] = chute.upper()
             index += 1
+        
+        print(formatar_lista(lista_letras_acertadas))
+        if (palavra_secreta.upper() == "".join(lista_letras_acertadas)):
+            print(f"Parabéns, você acertou a palavra secreta: {palavra_secreta.upper()}!")
+            break
 
     print("Fim de jogo")
+
+def formatar_lista(lista): return " ".join(lista)
 
 if __name__ == "__main__":
     jogar()
