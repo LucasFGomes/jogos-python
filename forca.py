83842a977f0aed4b327240b7b8845e64db0af546
Lucas Ferreira <lucasgomm33@gmail.com>
2020-05-08 03:16:15 -0300

    fix

--- forca.py
+++ forca.py
@@ -22,7 +22,7 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
-                lista_acertadas[index] = chute.upper()
+                lista_letras_acertadas[index] = chute.upper()
             index += 1
         
         print(formatar_lista(lista_letras_acertadas))
